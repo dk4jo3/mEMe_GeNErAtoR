9364099ef43ce312fb4654bd707d434524e66b25
dk4jo3 <dk4jo3@gmail.com>
2018-04-02 14:01:31 +0800

    fixed some stuff for python3

--- meme_gen.py
+++ meme_gen.py
@@ -1,11 +1,13 @@
 import random
 
 def memegen():
-  original = input("Enter input")
+  original = input("Enter input : ")
   c_list = []
+
   for c in original:
     c_list.append(c)
   meme_list = []
+
   for i in c_list:
     choice = random.choice([True, False])
     if choice == True:
@@ -13,8 +15,10 @@ def memegen():
       meme_list.append(i)
     else: 
       meme_list.append(i)
+
   meme_result = "".join(meme_list)
   print (meme_result)
-    
+
+
 memegen()
 
