e90aa2707a76bb53150ba07f5323d786cd306bf4
dk4jo3 <dk4jo3@gmail.com>
2020-07-05 08:45:31 +0800

    Update meme_gen.py

--- meme_gen.py
+++ meme_gen.py
@@ -1,23 +1,27 @@
 import random
 
 def memegen():
+
   original = input("Enter input : ")
   c_list = []
 
   for c in original:
     c_list.append(c)
-  meme_list = []
 
   for i in c_list:
+    meme_list = []
     choice = random.choice([True, False])
     if choice == True:
-      i = str.swapcase(i)
-      meme_list.append(i)
+      print (i)
+      #i = str.swapcase(i)
+      #meme_list.append(i)
     else: 
-      meme_list.append(i)
+      pass
+      #meme_list.append(i)
 
   meme_result = "".join(meme_list)
   print (meme_result)
+  print (meme_list)
 
 
 memegen()
